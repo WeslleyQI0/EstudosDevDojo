38dda41700c174796745413e1e5fa9e112aa64ae
Weslley <weslley,cvc@hotmail.com>
2017-02-26 22:27:41 -0300

    EstudosDevDojo Teste Git 2

--- src/OlaMundo.java
+++ src/OlaMundo.java
@@ -2,7 +2,7 @@
 public class OlaMundo {
     public static void main (String[] args){
         System.out.println("Olá diretamente do intellej");
-        System.out.println("Teste Git");
+        // System.out.println("Teste Git");
 
     }
 }
