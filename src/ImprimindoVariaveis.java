5f0c9e34bbdf0b76e7e1b961f3cdb6ae3f31c909
Weslley <weslley,cvc@hotmail.com>
2017-02-26 22:43:41 -0300

    Aula 05

--- src/ImprimindoVariaveis.java
+++ src/ImprimindoVariaveis.java
@@ -1,5 +1,12 @@
-/**
- * Created by Weslley on 26/02/2017.
+/*
+  Created by Weslley on 26/02/2017.
  */
 public class ImprimindoVariaveis {
+    public static void main(String[] args) {
+
+        int idade = 10 ;
+        int idadeDosPaisParaCadastro = 15;
+
+        System.out.println(idade&idadeDosPaisParaCadastro);
+    }
 }
