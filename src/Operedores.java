4342ae0bda65ec78651a51eb80a362133402d748
Weslley <weslley,cvc@hotmail.com>
2017-03-05 14:11:15 -0300

    Aula 06 Operadores

--- src/Operedores.java
+++ src/Operedores.java
@@ -3,6 +3,10 @@
  */
 public class Operedores {
     public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 20;
+
+        System.out.println(numero1+numero2);
 
     }
 }
