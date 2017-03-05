8705f70eccd05e4dbb04965e61fb988a874ff248
Weslley <weslley,cvc@hotmail.com>
2017-03-05 14:18:50 -0300

    Aula 06 Operadores

--- src/Operedores.java
+++ src/Operedores.java
@@ -5,8 +5,12 @@ public class Operedores {
     public static void main(String[] args) {
         int numero1 = 10;
         int numero2 = 20;
+        int soma;
 
-        System.out.println(numero1+numero2);
+        soma = numero1 + numero2;
 
+        System.out.println("A soma eh "+numero1+numero2); // Se o usarmos o operado "+" na hora de imprimir concatenado ele ira concatenar todas as variavei q tiver esse operador
+        System.out.println("A soma he "(+numero1+numero2));// Mais se quisermos fazer assim é melhor por as variaveis emtre parenteses
+        System.out.println("A soma eh "+soma); // O melhor é criar uma varivel que guarde o valor da soma
     }
 }
