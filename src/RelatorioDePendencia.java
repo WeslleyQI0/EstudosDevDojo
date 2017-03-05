5ca125d671b4cca06ca82c062c239ee055a56e1d
Weslley <weslley,cvc@hotmail.com>
2017-03-05 13:52:18 -0300

    Aula 06

---
+++ src/RelatorioDePendencia.java
@@ -0,0 +1,20 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Created by Weslley on 05/03/2017.
+ */
+public class RelatorioDePendencia {
+    public static void main(String[] args) {
+        String nome;
+        String endereco;
+        String telefone;
+
+        nome = "Weslley";
+        endereco  ="Rua Boa Vista";
+        telefone = "82991415255";
+
+
+        System.out.println("O "+nome+" domiciliado no endereço "+endereco+" e telefone "+telefone+" nao possue nenhum tipo de pendencia");
+    }
+
+}
