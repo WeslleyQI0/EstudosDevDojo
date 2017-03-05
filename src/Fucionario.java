c21f42307ce81162df5d45593efde63c8631ca06
Weslley <weslley,cvc@hotmail.com>
2017-03-05 14:04:00 -0300

    Aula 06

---
+++ src/Fucionario.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Weslley on 05/03/2017.
+ */
+public class Fucionario {
+    public static void main(String[] args) {
+        String nome;
+        int idade;
+        char sexo;
+        String estadoCivil;
+        double salario;
+
+        nome = "Wesley";
+        idade = 27;
+        sexo = 'M';
+        estadoCivil = "Casado";
+        salario = 1500;
+
+        System.out.println("O trabalhador(a) "+nome+ "\ndo sexo "+sexo+ " \nidade "+idade+" \nestado civil "+estadoCivil+" \ne salario "+salario+" \nenconta-se empregado nesse estabelecimento.");
+
+    }
+}
