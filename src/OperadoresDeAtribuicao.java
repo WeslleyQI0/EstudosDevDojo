920cad18fd01749c7d46ff9ce00dbc529e771080
Weslley <weslley,cvc@hotmail.com>
2017-03-13 21:50:15 -0300

    Aulas 08 e 09 Operadores de Atribuição e Logicos

---
+++ src/OperadoresDeAtribuicao.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Weslley on 13/03/2017.
+ */
+public class OperadoresDeAtribuicao {
+}
---
+++ src/OperadoresLogicos.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Weslley on 13/03/2017.
+ */
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        int idade = 18;
+        float salario = 1000f;
+
+        System.out.println(idade>=18 && salario >= 1000);
+    }
+}
